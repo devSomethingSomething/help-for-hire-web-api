8f5acde692c7e4df0d7d2ae6c7f09777ab80ab7d
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-11-02 14:48:22 +0200

    Add some parts of the test auth system

---
+++ HelpForHireWebApi/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using HelpForHireWebApi.Models;
+using HelpForHireWebApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpForHireWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<Auth> GetAuth(string id)
+        {
+            Auth auth = AuthService.GetAuth(id);
+
+            if (auth == null)
+            {
+                return NotFound();
+            }
+
+            return auth;
+        }
+    }
+}
