7810c014e2678edde01b48589b14b6cad0d25410
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-11-09 12:20:52 +0200

    Update user model

--- HelpForHireWebApi/Models/User.cs
+++ HelpForHireWebApi/Models/User.cs
@@ -1,22 +1,34 @@
-﻿using System;
+﻿using Google.Cloud.Firestore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HelpForHireWebApi.Models
 {
+    [FirestoreData]
     public abstract class User
     {
+        [Required]
         public string UserId { get; set; }
 
+        [Required]
+        [FirestoreProperty]
         public string Name { get; set; }
 
+        [Required]
+        [FirestoreProperty]
         public string Surname { get; set; }
 
+        [Required]
+        [FirestoreProperty]
         public string PhoneNumber { get; set; }
 
         // public Image ProfilePicture { get; set; }
 
-        public Location Location { get; set; }
+        [Required]
+        [FirestoreProperty]
+        public DocumentReference LocationId { get; set; }
     }
 }
