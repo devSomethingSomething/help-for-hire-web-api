0e5d6a3d37a499327972c14be7b6354d5848c5b6
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-11-22 11:41:12 +0200

    Add invite model and controller

---
+++ HelpForHireWebApi/Controllers/InviteController.cs
@@ -0,0 +1,128 @@
+﻿using Google.Cloud.Firestore;
+using HelpForHireWebApi.Managers;
+using HelpForHireWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpForHireWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InviteController : ControllerBase
+    {
+        private const string COLLECTION = "Invite";
+
+        public InviteController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Invite>> PostInvite(InviteDto inviteDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            CollectionReference collectionReference = FirestoreManager.Db
+                .Collection(COLLECTION);
+
+            await collectionReference.Document().SetAsync(inviteDto);
+
+            return CreatedAtAction(nameof(PostInvite), inviteDto);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Invite>> GetInvite(string id)
+        {
+            DocumentReference documentReference = FirestoreManager.Db
+                .Collection(COLLECTION).Document(id);
+
+            DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
+
+            if (documentSnapshot.Exists)
+            {
+                Invite invite = documentSnapshot.ConvertTo<Invite>();
+
+                invite.InviteId = id;
+
+                return Ok(invite);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("/api/[controller]/all")]
+        public async Task<ActionResult<List<Invite>>> GetInvites()
+        {
+            List<Invite> invites = new List<Invite>();
+
+            Query query = FirestoreManager.Db.Collection(COLLECTION);
+
+            QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
+            {
+                Invite invite = documentSnapshot.ConvertTo<Invite>();
+
+                invite.InviteId = documentSnapshot.Id;
+
+                invites.Add(invite);
+            }
+
+            if (invites.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(invites);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutInvite(string id, InviteDto inviteDto)
+        {
+            DocumentReference documentReference = FirestoreManager.Db
+                .Collection(COLLECTION).Document(id);
+
+            DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
+
+            if (documentSnapshot.Exists)
+            {
+                await documentReference.SetAsync(inviteDto, SetOptions.MergeAll);
+
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteInvite(string id)
+        {
+            DocumentReference documentReference = FirestoreManager.Db
+                .Collection(COLLECTION).Document(id);
+
+            DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
+
+            if (documentSnapshot.Exists)
+            {
+                await documentReference.DeleteAsync();
+
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- HelpForHireWebApi/Models/InviteDto.cs
+++ HelpForHireWebApi/Models/InviteDto.cs
@@ -8,22 +8,16 @@ using System.Threading.Tasks;
 namespace HelpForHireWebApi.Models
 {
     [FirestoreData]
-    public class Invite
+    public class InviteDto
     {
-        [Required]
-        public string InviteId { get; set; }
-
-        [Required]
         [FirestoreProperty]
         [StringLength(256)]
         public string Status { get; set; }
 
-        [Required]
         [FirestoreProperty]
         [StringLength(13, MinimumLength = 13)]
         public string EmployerId { get; set; }
 
-        [Required]
         [FirestoreProperty]
         [StringLength(13, MinimumLength = 13)]
         public string WorkerId { get; set; }
