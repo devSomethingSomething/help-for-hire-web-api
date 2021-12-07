2e545e5acbe78f9a779802be8e48604ff9af4686
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-12-07 15:21:35 +0200

    Add more code comments

--- HelpForHireWebApi/Controllers/InviteController.cs
+++ HelpForHireWebApi/Controllers/InviteController.cs
@@ -10,52 +10,83 @@ using System.Threading.Tasks;
 
 namespace HelpForHireWebApi.Controllers
 {
+    /// <summary>
+    /// This controller handles operations related
+    /// to invites received by workers from employers
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class InviteController : ControllerBase
     {
+        /// <summary>
+        /// Reference to the collection in the Firestore database
+        /// </summary>
         private const string COLLECTION = "Invite";
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public InviteController()
         {
 
         }
 
+        /// <summary>
+        /// Posts a new invite to the Firestore database
+        /// </summary>
+        /// <param name="inviteDto">The new invite to post</param>
+        /// <returns>Returns the created invite if successful otherwise returns a bad request</returns>
         [HttpPost]
         public async Task<ActionResult<Invite>> PostInvite(InviteDto inviteDto)
         {
+            // Check if the model is valid
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            // Perform a query to check if the invite already exists
+            // to prevent duplicates and abuse of the invite system
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("EmployerId", inviteDto.EmployerId)
                 .WhereEqualTo("WorkerId", inviteDto.WorkerId);
 
+            // Get a snapshot of the query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // If there is a document, return a bad request to indicate that a duplicate
+            // invite was found
             if (querySnapshot.Documents.Count != 0)
             {
                 return BadRequest("Duplicate invite detected");
             }
 
+            // Create a reference to the collection
             CollectionReference collectionReference = FirestoreManager.Db
                 .Collection(COLLECTION);
 
+            // Create the new invite in the Firestore collection
             await collectionReference.Document().SetAsync(inviteDto);
 
             return CreatedAtAction(nameof(PostInvite), inviteDto);
         }
 
+        /// <summary>
+        /// Gets a specific invite with the specified ID
+        /// </summary>
+        /// <param name="id">The ID of the invite to retrieve</param>
+        /// <returns>The found invite otherwise a not found result</returns>
         [HttpGet]
         public async Task<ActionResult<Invite>> GetInvite(string id)
         {
+            // Get a reference to the invite we are searching for
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
+            // Get a snapshot to the invite document
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the document exists, return it, otherwise return not found
             if (documentSnapshot.Exists)
             {
                 Invite invite = documentSnapshot.ConvertTo<Invite>();
@@ -70,15 +101,23 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Gets all the invites in the invite collection
+        /// </summary>
+        /// <returns>Returns a list of invites if any are found otherwise returns not found</returns>
         [HttpGet("/api/[controller]/all")]
         public async Task<ActionResult<List<Invite>>> GetInvites()
         {
+            // List of invite results from the query
             List<Invite> invites = new List<Invite>();
 
+            // Perform a query to retrieve all entries in the Firestore collection
             Query query = FirestoreManager.Db.Collection(COLLECTION);
 
+            // Get a snapshot of this query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // For every retrieved document, convert it to an invite and add it to the results list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Invite invite = documentSnapshot.ConvertTo<Invite>();
@@ -88,6 +127,7 @@ namespace HelpForHireWebApi.Controllers
                 invites.Add(invite);
             }
 
+            // If no invites were found, return a not found result
             if (invites.Count == 0)
             {
                 return NotFound();
@@ -96,14 +136,23 @@ namespace HelpForHireWebApi.Controllers
             return Ok(invites);
         }
 
+        /// <summary>
+        /// Updates a specific invite
+        /// </summary>
+        /// <param name="id">The ID of the invite we want to update</param>
+        /// <param name="inviteDto">The new invite information to use in the update</param>
+        /// <returns>No content if successful otherwise a not found result</returns>
         [HttpPut]
         public async Task<ActionResult> PutInvite(string id, InviteDto inviteDto)
         {
+            // Get a reference to the document we want to update
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
+            // Get a snapshot of the document
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the document exists, we update it otherwise we return not found
             if (documentSnapshot.Exists)
             {
                 await documentReference.SetAsync(inviteDto, SetOptions.MergeAll);
@@ -116,14 +165,22 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Deletes an invite
+        /// </summary>
+        /// <param name="id">The ID of the invite that we want to delete</param>
+        /// <returns>No content if successful otherwise a not found result</returns>
         [HttpDelete]
         public async Task<ActionResult> DeleteInvite(string id)
         {
+            // Get a reference to the document we want to delete
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
+            // Get a snapshot of the document
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the document exists, then delete it otherwise return not found
             if (documentSnapshot.Exists)
             {
                 await documentReference.DeleteAsync();
@@ -136,16 +193,27 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Gets invites for a specific employer
+        /// </summary>
+        /// <param name="employerId">The ID of the employer</param>
+        /// <returns>A list of invites if any are found otherwise a not found result</returns>
         [HttpGet("/api/[controller]/employer")]
         public async Task<ActionResult<List<Invite>>> GetInvitesForEmployer(string employerId)
         {
+            // The list of invites to return
             List<Invite> invites = new List<Invite>();
 
+            // Perform a query to find invites where the employer ID is equal to the one
+            // provided to the method
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("EmployerId", employerId);
 
+            // Get a snapshot of the query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // For each found document, convert it to an invite and add it to the invites
+            // list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Invite invite = documentSnapshot.ConvertTo<Invite>();
@@ -155,6 +223,7 @@ namespace HelpForHireWebApi.Controllers
                 invites.Add(invite);
             }
 
+            // If no invites were found, return a not found result
             if (invites.Count == 0)
             {
                 return NotFound();
@@ -163,16 +232,27 @@ namespace HelpForHireWebApi.Controllers
             return Ok(invites);
         }
 
+        /// <summary>
+        /// Gets invites for a specific worker account
+        /// </summary>
+        /// <param name="workerId">The ID of the worker</param>
+        /// <returns>A list of invites if any are found otherwise a not found result</returns>
         [HttpGet("/api/[controller]/worker")]
         public async Task<ActionResult<List<Invite>>> GetInvitesForWorker(string workerId)
         {
+            // List of invites to return
             List<Invite> invites = new List<Invite>();
 
+            // Perform a query to find invites where the worker ID matches the provided
+            // ID
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("WorkerId", workerId);
 
+            // Get a snapshot of the query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // Convert each found document into an invite and add it to the results
+            // list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Invite invite = documentSnapshot.ConvertTo<Invite>();
@@ -182,6 +262,7 @@ namespace HelpForHireWebApi.Controllers
                 invites.Add(invite);
             }
 
+            // Return not found if no invites were found
             if (invites.Count == 0)
             {
                 return NotFound();
--- HelpForHireWebApi/Controllers/JobController.cs
+++ HelpForHireWebApi/Controllers/JobController.cs
@@ -10,51 +10,80 @@ using System.Threading.Tasks;
 
 namespace HelpForHireWebApi.Controllers
 {
+    /// <summary>
+    /// This controller handles all operations related
+    /// to jobs
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class JobController : ControllerBase
     {
+        /// <summary>
+        /// Reference to the collection in the Firestore database
+        /// </summary>
         private const string COLLECTION = "Job";
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public JobController()
         {
 
         }
 
+        /// <summary>
+        /// Creates a new job
+        /// </summary>
+        /// <param name="jobDto">The job data to post</param>
+        /// <returns>The created job otherwise a bad request</returns>
         [HttpPost]
         public async Task<ActionResult<Job>> PostJob(JobDto jobDto)
         {
+            // Check to see if the model is valid first
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            // Perform a query to check if the job already exists
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("Title", jobDto.Title);
 
+            // Get a snapshot of the query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // If a duplicate is found, then return a bad request
             if (querySnapshot.Documents.Count != 0)
             {
                 return BadRequest("Duplicate item detected");
             }
 
+            // Get a reference to the collection
             CollectionReference collectionReference = FirestoreManager.Db
                 .Collection(COLLECTION);
 
+            // Create the new job
             await collectionReference.Document().SetAsync(jobDto);
 
             return CreatedAtAction(nameof(PostJob), jobDto);
         }
 
+        /// <summary>
+        /// Gets a specific job
+        /// </summary>
+        /// <param name="id">The ID of the job we want to retrieve</param>
+        /// <returns>The found job otherwise a not found result</returns>
         [HttpGet]
         public async Task<ActionResult<Job>> GetJob(string id)
         {
+            // Create a reference to the job we are looking for
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
+            // Get a snapshot of the document
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the job exists, return it otherwise return a not found result
             if (documentSnapshot.Exists)
             {
                 Job job = documentSnapshot.ConvertTo<Job>();
@@ -69,15 +98,23 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Gets all the jobs in the database
+        /// </summary>
+        /// <returns>A list of jobs otherwise a not found result</returns>
         [HttpGet("/api/[controller]/all")]
         public async Task<ActionResult<List<Job>>> GetJobs()
         {
+            // List of jobs to return
             List<Job> jobs = new List<Job>();
 
+            // Perform a query to retrieve all the jobs in the collection
             Query query = FirestoreManager.Db.Collection(COLLECTION);
 
+            // Get a snapshot of the query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // For every found job, add it to the jobs list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Job job = documentSnapshot.ConvertTo<Job>();
@@ -87,6 +124,7 @@ namespace HelpForHireWebApi.Controllers
                 jobs.Add(job);
             }
 
+            // If no jobs were found, return a not found result
             if (jobs.Count == 0)
             {
                 return NotFound();
@@ -95,15 +133,25 @@ namespace HelpForHireWebApi.Controllers
             return Ok(jobs);
         }
 
+        /// <summary>
+        /// Get selected jobs for a user
+        /// </summary>
+        /// <param name="jobIds">The list of jobs we want to retrieve</param>
+        /// <returns>A list of jobs otherwise a not found result</returns>
         [HttpGet("/api/[controller]/selected")]
-        public async Task<ActionResult<List<Job>>> GetSelectedJobs(String[] ids)
+        public async Task<ActionResult<List<Job>>> GetSelectedJobs(string[] jobIds)
         {
+            // List of jobs to return
             List<Job> jobs = new List<Job>();
 
-            Query query = FirestoreManager.Db.Collection(COLLECTION).WhereArrayContainsAny("JobId",ids);
+            // Get a list of jobs based off of the IDs provided to the method
+            Query query = FirestoreManager.Db.Collection(COLLECTION)
+                .WhereArrayContainsAny("JobId", jobIds);
 
+            // Get a snapshot of the query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // For any found jobs, add them to the results list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Job job = documentSnapshot.ConvertTo<Job>();
@@ -113,6 +161,7 @@ namespace HelpForHireWebApi.Controllers
                 jobs.Add(job);
             }
 
+            // If no jobs were found, return a not found result
             if (jobs.Count == 0)
             {
                 return NotFound();
@@ -121,24 +170,36 @@ namespace HelpForHireWebApi.Controllers
             return Ok(jobs);
         }
 
+        /// <summary>
+        /// Updates a job
+        /// </summary>
+        /// <param name="id">The ID of the job we want to update</param>
+        /// <param name="jobDto">The new job data to use in the update</param>
+        /// <returns>No content if successful otherwise not found</returns>
         [HttpPut]
         public async Task<ActionResult> PutJob(string id, JobDto jobDto)
         {
+            // Perform a query to search for duplicate jobs
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("Title", jobDto.Title);
 
+            // Get a snapshot of this query
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // If any duplicates were found, return a bad request
             if (querySnapshot.Documents.Count != 0)
             {
                 return BadRequest("Duplicate item detected");
             }
 
+            // Get a reference to the collection
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
+            // Get a snapshot of this document
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the document exists, perform an update otherwise return a not found result
             if (documentSnapshot.Exists)
             {
                 await documentReference.SetAsync(jobDto, SetOptions.MergeAll);
@@ -151,14 +212,22 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Deletes a job
+        /// </summary>
+        /// <param name="id">The ID of the job we want to delete</param>
+        /// <returns>No content if successful otherwise a not found result</returns>
         [HttpDelete]
         public async Task<ActionResult> DeleteJob(string id)
         {
+            // Get a reference to the document with the provided ID
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
+            // Get a snapshot of this document
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the document exists, delete it otherwise return not found
             if (documentSnapshot.Exists)
             {
                 await documentReference.DeleteAsync();
