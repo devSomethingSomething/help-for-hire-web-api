d2ede07fc62f6827ca0ee8740f6e8192cbacd0be
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2021-12-08 10:58:16 +0200

    Add final comments

--- HelpForHireWebApi/Controllers/ReportController.cs
+++ HelpForHireWebApi/Controllers/ReportController.cs
@@ -10,31 +10,49 @@ using System.Threading.Tasks;
 
 namespace HelpForHireWebApi.Controllers
 {
+    /// <summary>
+    /// This controller handles operations related to
+    /// reports
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class ReportController : ControllerBase
     {
+        /// <summary>
+        /// Reference to the collection in the Firestore database
+        /// </summary>
         private const string COLLECTION = "Report";
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public ReportController()
         {
 
         }
 
+        /// <summary>
+        /// Creates a new report in the Firestore database
+        /// </summary>
+        /// <param name="reportDto">The new report to post</param>
+        /// <returns>The created report otherwise a bad request result is returned</returns>
         [HttpPost]
         public async Task<ActionResult<Report>> PostReport(ReportDto reportDto)
         {
+            // If the report is not valid, return a bad request
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            // Perform a query to check if the report is a duplicate
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("ReportedUserId", reportDto.ReportedUserId)
                 .WhereEqualTo("ReporterUserId", reportDto.ReporterUserId);
 
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // If a duplicate report is found, return a bad request with the error
             if (querySnapshot.Documents.Count != 0)
             {
                 return BadRequest("Duplicate report detected");
@@ -43,20 +61,29 @@ namespace HelpForHireWebApi.Controllers
             CollectionReference collectionReference = FirestoreManager.Db
                 .Collection(COLLECTION);
 
+            // Create the new report in the database
             await collectionReference.Document().SetAsync(reportDto);
-
+            
+            // Return the created report
             return CreatedAtAction(nameof(PostReport), reportDto);
         }
 
+        /// <summary>
+        /// Gets all the reports from the Firestore database
+        /// </summary>
+        /// <returns>A list of found reports if any exist, if none are found then a not found request is returned</returns>
         [HttpGet("/api/[controller]/all")]
         public async Task<ActionResult<List<Report>>> GetReports()
         {
+            // Found reports to return
             List<Report> reports = new List<Report>();
 
+            // Query the collection and get back all reports
             Query query = FirestoreManager.Db.Collection(COLLECTION);
 
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // Each document gets converted to a report and added to the results list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Report report = documentSnapshot.ConvertTo<Report>();
@@ -66,22 +93,32 @@ namespace HelpForHireWebApi.Controllers
                 reports.Add(report);
             }
 
+            // If no reports are found, then return a not found result
             if (reports.Count == 0)
             {
                 return NotFound();
             }
 
+            // Return reports if any were found
             return Ok(reports);
         }
 
+        /// <summary>
+        /// Deletes a single report from the database
+        /// </summary>
+        /// <param name="id">The ID of the report to delete</param>
+        /// <returns>No content if successful otherwise not found if no report was found</returns>
         [HttpDelete]
         public async Task<ActionResult> DeleteReport(string id)
         {
+            // Attempt to reference a document with the provided ID
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the report document exists, delete it, otherwise if it is not found then
+            // return not found
             if (documentSnapshot.Exists)
             {
                 await documentReference.DeleteAsync();
--- HelpForHireWebApi/Controllers/WorkerController.cs
+++ HelpForHireWebApi/Controllers/WorkerController.cs
@@ -10,20 +10,36 @@ using System.Threading.Tasks;
 
 namespace HelpForHireWebApi.Controllers
 {
+    /// <summary>
+    /// This controller handles operations for worker
+    /// account types
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class WorkerController : ControllerBase
     {
+        /// <summary>
+        /// Reference to the collection in the Firestore database
+        /// </summary>
         private const string COLLECTION = "Worker";
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public WorkerController()
         {
 
         }
 
+        /// <summary>
+        /// Creates a new worker in the Firestore database
+        /// </summary>
+        /// <param name="worker">The new worker to be posted</param>
+        /// <returns>The newly created worker otherwise a bad request if there is an error</returns>
         [HttpPost]
         public async Task<ActionResult<Worker>> PostWorker(Worker worker)
         {
+            // If the model is not valid, then return a bad request with the error
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -32,19 +48,29 @@ namespace HelpForHireWebApi.Controllers
             CollectionReference collectionReference = FirestoreManager.Db
                     .Collection(COLLECTION);
 
+            // Create the new worker in the database
             await collectionReference.Document(worker.UserId).SetAsync(worker);
 
+            // Return the created worker object
             return CreatedAtAction(nameof(PostWorker), worker);
         }
 
+        /// <summary>
+        /// Gets a single worker object back
+        /// </summary>
+        /// <param name="id">The ID of the worker to retrieve</param>
+        /// <returns>A worker object if found, otherwise a not found result</returns>
         [HttpGet]
         public async Task<ActionResult<Worker>> GetWorker(string id)
         {
+            // Attempt to find the worker document
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the worker document exists, return a worker object, otherwise
+            // return a not found result
             if (documentSnapshot.Exists)
             {
                 Worker worker = documentSnapshot.ConvertTo<Worker>();
@@ -59,15 +85,22 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Gets all the worker objects from the database
+        /// </summary>
+        /// <returns>A list of worker objects if any are found, otherwise a not found result</returns>
         [HttpGet("/api/[controller]/all")]
         public async Task<ActionResult<List<Worker>>> GetWorkers()
         {
+            // The list of workers to return
             List<Worker> workers = new List<Worker>();
 
+            // Perform a query to retrieve all the worker documents
             Query query = FirestoreManager.Db.Collection(COLLECTION);
 
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // Convert each found document to a worker object and add it to the results list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Worker worker = documentSnapshot.ConvertTo<Worker>();
@@ -77,22 +110,33 @@ namespace HelpForHireWebApi.Controllers
                 workers.Add(worker);
             }
 
+            // Return a not found result if no worker documents were found
             if (workers.Count == 0)
             {
                 return NotFound();
             }
 
+            // Return any found worker objects
             return Ok(workers);
         }
 
+        /// <summary>
+        /// Updates a single worker document
+        /// </summary>
+        /// <param name="id">The ID of the worker to update</param>
+        /// <param name="workerDto">The new worker data to use for the update</param>
+        /// <returns>No content if successful otherwise a not found result if no worker was found</returns>
         [HttpPut]
         public async Task<ActionResult> PutWorker(string id, WorkerDto workerDto)
         {
+            // Attempt to find the worker with the provided ID
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the worker exists, perform an update on their details, otherwise
+            // return a not found result
             if (documentSnapshot.Exists)
             {
                 await documentReference.SetAsync(workerDto, SetOptions.MergeAll);
@@ -105,14 +149,22 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Deletes a single worker document
+        /// </summary>
+        /// <param name="id">The ID of the worker to be deleted</param>
+        /// <returns>No content if successful, otherwise a not found result</returns>
         [HttpDelete]
         public async Task<ActionResult> DeleteWorker(string id)
         {
+            // Attempt to find the worker first before deletion
             DocumentReference documentReference = FirestoreManager.Db
                 .Collection(COLLECTION).Document(id);
 
             DocumentSnapshot documentSnapshot = await documentReference.GetSnapshotAsync();
 
+            // If the worker does exist, delete their data, otherwise
+            // return a not found result
             if (documentSnapshot.Exists)
             {
                 await documentReference.DeleteAsync();
@@ -125,16 +177,24 @@ namespace HelpForHireWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Gets all the workers in a certain location
+        /// </summary>
+        /// <param name="locationId">The location to find workers at</param>
+        /// <returns>A list of workers if any are found, otherwise a not found result if none exist</returns>
         [HttpGet("/api/[controller]/cities")]
         public async Task<ActionResult<List<Worker>>> GetWorkersInCity(string locationId)
         {
+            // The list of workers to be returned
             List<Worker> workers = new List<Worker>();
 
+            // Find all workers at the specified location
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("LocationId", locationId);
 
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // Convert each found document into a worker object and add it to the results list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Worker worker = documentSnapshot.ConvertTo<Worker>();
@@ -144,26 +204,39 @@ namespace HelpForHireWebApi.Controllers
                 workers.Add(worker);
             }
 
+            // If no workers were found, then return a not found result
             if (workers.Count == 0)
             {
                 return NotFound();
             }
 
+            // Return the workers found at a specific location
             return Ok(workers);
         }
 
         // This will change to skills later, for now use the job IDs
+        /// <summary>
+        /// Gets all the workers who can perform certain jobs
+        /// </summary>
+        /// <param name="locationId">The location to search for</param>
+        /// <param name="jobIds">The jobs to search for</param>
+        /// <returns>A list of workers if any are found, otherwise a not found result</returns>
         [HttpGet("/api/[controller]/skills")]
         public async Task<ActionResult<List<Worker>>> GetWorkersWithSkills(string locationId, [FromQuery]List<string> jobIds)
         {
+            // The list of worker results to return
             List<Worker> workers = new List<Worker>();
 
+            // Query the database to find any workers in the specified location
+            // and who have the right skills
             Query query = FirestoreManager.Db.Collection(COLLECTION)
                 .WhereEqualTo("LocationId", locationId)
                 .WhereArrayContainsAny("JobIds", jobIds);
 
             QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
 
+            // Convert any found worker documents to a worker object and add it
+            // to the results list
             foreach (DocumentSnapshot documentSnapshot in querySnapshot.Documents)
             {
                 Worker worker = documentSnapshot.ConvertTo<Worker>();
@@ -173,11 +246,13 @@ namespace HelpForHireWebApi.Controllers
                 workers.Add(worker);
             }
 
+            // If no documents were found, return a not found result
             if (workers.Count == 0)
             {
                 return NotFound();
             }
 
+            // Return any found worker objects
             return Ok(workers);
         }
     }
